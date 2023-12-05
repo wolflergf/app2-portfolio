c71ac53c62d91bdaf601e4b885b232474592c687
wolflergf <wolflergf@gmail.com>
2023-12-05 18:56:06 +0000

    Delete files, create a new layout, update photo

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("./images/photo.png")
+    st.image("./images/photo.jpg")
 
 with col2:
-    st.title("Ardit Sulce")
+    st.title("Wolfler Guzzo Ferreira")
     content = """
-    Hi, I am Ardit! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
-I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
+    Lorem incididunt consectetur excepteur ea exercitation amet consectetur quis ea aliquip do. Occaecat deserunt velit nisi anim 
+    fugiat in aliquip exercitation. Fugiat ea occaecat laboris eiusmod cillum minim. Deserunt fugiat consectetur eu laborum.
+    Aute aliquip sint proident minim magna cillum dolore officia. Ex dolore esse amet consequat. Duis pariatur irure Lorem 
+    incididunt Lorem magna cupidatat. Veniam in eu incididunt exercitation incididunt ea nostrud. Enim duis magna cupidatat 
+    occaecat laboris ut. Quis nisi sunt consectetur irure non aliqua ea Lorem culpa.
     """
     st.info(content)
 
